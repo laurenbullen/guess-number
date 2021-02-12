4927de34dbc86e6c0ab914eec3f2dc9fff2cb0f5
Lauren Bullen <laurenbullen@comcast.net>
2021-02-12 09:54:05 -0800

    added js functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// this makes the random number 1-20 appear in header
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number added!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent =
+      "You guessed the correct number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Your guess was too high";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Your guess was too low";
+  }
+});
