4c430193bb0ee521ef34277edf0960fcd2c9b37a
Lauren Bullen <laurenbullen@comcast.net>
2021-02-23 19:43:51 -0800

    added some css and js

--- script.js
+++ script.js
@@ -10,17 +10,26 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = document.querySelector(".guess").value;
   console.log(guess, typeof guess);
 
+  //this happens if no number is entered
   if (!guess) {
     document.querySelector(".message").textContent = "No number added!";
+
+    //happens if number is correct
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent =
       "You guessed the correct number!";
     score++;
     document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#rgb";
+
+    //happens if guess is too high
   } else if (guess > secretNumber) {
     document.querySelector(".message").textContent = "Your guess was too high";
     score--;
     document.querySelector(".score").textContent = score;
+
+    //happens if guess is too low
   } else if (guess < secretNumber) {
     document.querySelector(".message").textContent = "Your guess was too low";
     score--;
